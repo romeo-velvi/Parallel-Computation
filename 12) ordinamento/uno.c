07f18dcdb3a938b73eaa5658e161368838627b75
ROMEK <ubuntu@ubuntu2004.linuxvmimages.local>
2021-07-13 17:10:31 +0200

    order add

--- 12) ordinamento/uno.c
+++ 12) ordinamento/uno.c
@@ -1,74 +1,178 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <omp.h>
+#define N 50
+#define t 4
 
+void oddEvenSort(int *a){
+	int sw=1, start=0, i=0, tmp;
+	while(sw || start){
+		sw=0;
+		#pragma omp parallel for private(tmp)
+		for(i=start; i<N-1;i+=2){
+			if(a[i]>a[i+1]){
+				tmp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=tmp;
+				sw = 1;
+			}
+		}
+		if(start == 0)
+			start = 1;
+		else
+			start = 0;
+	}
+}
 
-int binarySearch(int left, int right,int t,int token, int *a);
-
-int main()
-{
-
-    int n, i, token, t = 3, result, *a;
-
-    printf("Inserire la dimensione del vettore: ");
-    scanf("%d", &n);
-
-    a = (int *)malloc(n*sizeof(int));
-
-    for(i = 0; i < n; i++)
-    {
-        a[i] = i;
-        printf("[%d]\t", a[i]);
+void merge(int arr[], int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+ 
+void mergeSort(int arr[], int l, int r){
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
 
-    printf("\nInserire il numero da cercare: ");
-    scanf("%d", &token);
 
-     omp_set_num_threads(t);
+void superSort(int *r){
+	int i,resto,step,id,index1,index2,nloc;
+	
+	#pragma omp parallel private(i,resto,step,id,index1,index2) num_threads(t)
+	{
+		nloc=N/t; //si calcola quanti (interamente) saranno gli addendi per thread 
+		resto=N%t; // si calcola il resto della divisione
+		id = omp_get_thread_num();
+		if(id<resto){ //se l'id del thread è < del resto
+			nloc++; // deve avere un addendo in più
+			step=0;
+		}else{
+			step=resto;
+		}
+		
+			index1= 0 + nloc * omp_get_thread_num() + step;
+			index2= (nloc-1) + nloc * omp_get_thread_num() + step;
+			mergeSort(r,index1,index2);
+			//printf("\n Merged from %d to %d \n",index1,index2);
+	}
+	
+	int mee=0;
+	
+	for(i=0;i<t;i++){
+		nloc=N/t; //si calcola quanti (interamente) saranno gli addendi per thread 
+		resto=N%t; // si calcola il resto della divisione
+		id = omp_get_thread_num();
+		if(id<resto){ //se l'id del thread è < del resto
+			nloc++; // deve avere un addendo in più
+			step=0;
+		}else{
+			step=resto;
+		}
+		index1 = nloc * i + step;
+		index2 = (nloc-1) + nloc * i + step;
+		mee=(index1+index2)/2;
+		merge(r,index1,mee,index2);
+	}
 
-    double t0 = omp_get_wtime();
-    result = binarySearch(0, n - 1, t, token, a);
-    double t1 = omp_get_wtime();
+}
 
-    if (result < 0)
-        printf("The number not is in the vector.\n");
-    else
-        printf("The number is in the position: %d\n", result);
 
-    printf("TEMPO %.6f\n",t1-t0);
+
+void stampavettore(int *r, char c){
+	int i;
+	for(i=0;i<N;i++){
+		if(c=='q'){
+			printf("q[%d]=%d\n",i,r[i]);
+		}
+		else{
+			printf("r[%d]=%d\n",i,r[i]);
+		}
+	}
+}
+
+int main(){
+
+	int *r, *q, *T, i,j, temp;
+	
+	r=(int*)malloc((N)*sizeof(int));
+	q=(int*)malloc((N)*sizeof(int));
+	T=(int*)malloc((N)*sizeof(int));
+	
+	srand(time(NULL));
+
+	for(i=0;i<N;i++){ // array da 0 a N-1
+		r[i]=rand()%N;
+		//printf("r[%d]=%d\n",i,r[i]);
+	}
+	printf("\n\n");
+	for(i=0;i<N;i++){ // array da 0 a N-1
+		q[i]=rand()%N;
+		//printf("q[%d]=%d\n",i,q[i]);
+	}
+	printf("\n\n");
+    
+    omp_set_num_threads(t);
+    double t0,t1;
+
+    t0 = omp_get_wtime();
+    mergeSort(r,0,N-1);       
+    t1 = omp_get_wtime();
+    printf("TEMPO Merge sort %.6f\n",t1-t0);
+    //stampavettore(r,'r');
+    
+	t0 = omp_get_wtime();
+    superSort(q);
+    t1 = omp_get_wtime();
+    printf("TEMPO Super sort %.6f\n",t1-t0);
+    //stampavettore(q,'q');
+    
+    t0 = omp_get_wtime();
+    oddEvenSort(q);
+   	t1 = omp_get_wtime();
+    printf("TEMPO Odd-Even sort %.6f\n",t1-t0);
+    //stampavettore(q,'q');
+        
+    
+    
     return 0;
 }
 
 
 
-int binarySearch(int left, int right,int t, int token, int *a)
-{   
-    if(token<a[left] || token>a[right])
-        return -1;
-
-    int result;
-    int leftThread,rightThread,id;
-
-    int mid = (right - left + 1)/2; //Sottoporzione del vettore.
-
-
-    if (mid == 0)  //Se il vettore è composto da un solo elemento.
-        return (a[left] != token) ?  -1 :  left;
-
-    if(a[mid] == token)
-        return a[mid];
-
-    omp_set_nested(1);
-
-
-    #pragma omp parallel shared(a, token, left, right, mid, result) private(leftThread,rightThread,id)
-    {
-         id = omp_get_thread_num();
-         leftThread = left + id * mid;
-         rightThread = (id == t-1) ? right : leftThread + mid - 1;
-
-        if(a[leftThread] <= token && a[rightThread] >= token)
-             result = binarySearch(leftThread, rightThread,t,token, a);
-        
-    }
-}
